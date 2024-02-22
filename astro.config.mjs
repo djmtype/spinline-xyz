a18a0f34161bd745fa334bcdb7e429bc1f465867
Scott Rod <scott@spinlinestudio.com>
2024-02-22 14:04:31 -0500

    Updated form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import lighthouse from "astro-lighthouse";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://spinline.xyz',
-	integrations: [mdx(), sitemap()],
+  site: 'https://spinline.xyz',
+  integrations: [mdx(), sitemap(), lighthouse()]
 });
